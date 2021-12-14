fb712e1ef20323660eb73cd6c68a0b5192618807
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-12-13 21:15:58 -0400

    Add propTypes to Project component

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,7 +1,22 @@
-import react from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Project = () => (
-  <section />
+const Project = ({
+  coverImg,
+  title,
+  desc,
+}) => (
+  <section>
+    <img src={coverImg} alt="cover-image" />
+    <h3>{title}</h3>
+    <p>{desc}</p>
+  </section>
 );
+
+Project.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+}
 
 export default Project;
