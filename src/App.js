3d33210222eac8d51c27ed03ed3adb51120a9969
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-12-15 22:50:01 -0400

    Add one project component to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Project from './components/project/Project';
+import jsImg from './images/todo.png';
 
 function App() {
   return (
     <div className="App">
-      
+      <Project coverImg={jsImg} title='Todo List App' desc='A JavaScript app that allows
+      users to add tasks to a to-do list, edit existing tasks, mark tasks as completed,
+      and delete existing tasks.' />
     </div>
   );
 }
