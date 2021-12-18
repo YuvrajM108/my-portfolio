b84e199202b53167a921824edcaaea3436698a75
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-12-17 21:58:20 -0400

    Add React Project component to App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Project from './components/project/Project';
 import jsImg from './images/todo.png';
 import rorImg from './images/groups-index-screenshot.png';
+import reactImg from './images/carbon-intensity-home-screenshot.png';
 
 function App() {
   return (
@@ -9,9 +10,16 @@ function App() {
       <Project coverImg={jsImg} title='Todo List App' desc='A JavaScript app that allows 
       users to add tasks to a to-do list, edit existing tasks, mark tasks as completed, 
       and delete existing tasks.' />
-      <Project coverImg={rorImg} title='University Project Time Tracker' desc='A Ruby On Rails app that 
-      allows a user to keep track of the hours that they have spent on specific projects at their 
-      university. It also allows them to group these projects by subject.' />
+      <Project coverImg={rorImg}
+      title='University Project Time Tracker'
+      desc='A Ruby On Rails app that allows a user to keep track of the hours that 
+      they have spent on specific projects at their university. It also allows them 
+      to group these projects by subject.' />
+      <Project coverImg={reactImg}
+      title='Carbon Intensity Tracker'
+      desc='A Ruby On Rails app that allows a user to keep track of the hours that 
+      they have spent on specific projects at their university. It also allows them 
+      to group these projects by subject.' />
     </div>
   );
 }
