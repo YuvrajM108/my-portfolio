be6e2e3aab8c78d496244914ae275b63c9d3a693
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-12-23 21:46:06 -0400

    Fix image errors

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import './App.css';
 import Project from './components/project/Project';
-import jsImg from './images/todolist.png';
-import rorImg from './images/groups-index-screenshot.png';
-import reactImg from './images/carbon-intensity-home-screenshot.png';
-import railsReactImg from './images/car-reservations.png';
 
 function App() {
   return (
     <div className="App">
-      <Project coverImg={jsImg}
+      <Project coverImg="./images/todolist.png"
       title='Todo List App'
       desc='A JavaScript app that allows 
       users to add tasks to a to-do list, edit existing tasks, mark tasks as completed, 
       and delete existing tasks.' />
-      <Project coverImg={rorImg}
+      <Project coverImg="./images/groups-index-screenshot.png"
       title='University Project Time Tracker'
       desc='A Ruby On Rails app that allows a user to keep track of the hours that 
       they have spent on specific projects at their university. It also allows them 
       to group these projects by subject.' />
-      <Project coverImg={reactImg}
+      <Project coverImg="./images/carbon-intensity-home-screenshot.png"
       title='Carbon Intensity Tracker'
       desc='A simple React application that fetches and displays data about the carbon
        intensity of the various districts in Great Britain.' />
-      <Project coverImg={railsReactImg}
+      <Project coverImg="./images/car-reservations.png"
       title='Car Reservation Website' 
       desc='A web application with a React front end and a Rails API back end that allows the user to 
       add cars to the catalogue, delete cars, and reserve cars.' />
