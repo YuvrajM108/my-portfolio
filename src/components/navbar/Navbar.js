35ab401e13fd9e6134d097a71552e64d72819f27
YuvrajM108 <yuvrajmanoo1@gmail.com>
2022-09-27 17:33:44 -0400

    Attempt to fix eslint errors

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-const Navbar = () => {
-  
-  
-  return (
-    <nav className="navbar">
-      <a href="#bio" className="navlink">ABOUT ME</a>
-      <a href="#projects" className="navlink">PROJECTS</a>
-      <a href="#contact" className="navlink">CONTACT</a>
-    </nav>
-  );
-};
+const Navbar = () => (
+  <nav className="navbar">
+    <a href="#bio" className="navlink">ABOUT ME</a>
+    <a href="#projects" className="navlink">PROJECTS</a>
+    <a href="#contact" className="navlink">CONTACT</a>
+  </nav>
+);
 
 export default Navbar;
