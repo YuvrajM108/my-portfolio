3b9813e2044ad3d6b7e9074766eb14ba7fef5d38
YuvrajM108 <yuvrajmanoo1@gmail.com>
2022-09-28 16:48:54 -0400

    Attempt to add responsiveness to about link

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Navbar = () => (
-  <nav className="navbar">
-    <a href="#bio" className="navlink">ABOUT ME</a>
-    <a href="#projects" className="navlink">PROJECTS</a>
-    <a href="#contact" className="navlink">CONTACT</a>
-  </nav>
-);
+const Navbar = () => {
+  const [about, setAbout] =useState(false);
+
+  const showAbout = () => {
+    if(window.scrollY >= 100) {
+      setAbout(true);
+    } else {
+      setAbout(false);
+    }
+  };
+
+  window.addEventListener('scroll', showAbout);
+
+  return (
+    <nav className="navbar">
+      <a href="#bio" className={about ? "navlink" : "hiddenlink"}>ABOUT ME</a>
+      <a href="#projects" className="navlink">PROJECTS</a>
+      <a href="#contact" className="navlink">CONTACT</a>
+    </nav>
+  )
+};
 
 export default Navbar;
