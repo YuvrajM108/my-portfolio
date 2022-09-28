1d526d785812f1cd8ecdf3e1f6682eb071ce0e28
YuvrajM108 <yuvrajmanoo1@gmail.com>
2022-09-28 16:51:28 -0400

    Attempt to fix eslint errors

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
 const Navbar = () => {
-  const [about, setAbout] =useState(false);
+  const [about, setAbout] = useState(false);
 
   const showAbout = () => {
-    if(window.scrollY >= 100) {
+    if (window.scrollY >= 100) {
       setAbout(true);
     } else {
       setAbout(false);
@@ -15,11 +15,11 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <a href="#bio" className={about ? "navlink" : "hiddenlink"}>ABOUT ME</a>
+      <a href="#bio" className={about ? 'navlink' : 'hiddenlink'}>ABOUT ME</a>
       <a href="#projects" className="navlink">PROJECTS</a>
       <a href="#contact" className="navlink">CONTACT</a>
     </nav>
-  )
+  );
 };
 
 export default Navbar;
