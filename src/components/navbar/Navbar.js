a8fcc7c391722bc300d36795bd92ec5b9ed8ec98
YuvrajM108 <yuvrajmanoo1@gmail.com>
2022-10-31 18:04:46 -0400

    Attempt to fix eslint error

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -16,8 +16,8 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       {
-      about ? <a href="#bio" className="navlink">ABOUT ME</a> :
-      <p> </p>
+      about ? <a href="#bio" className="navlink">ABOUT ME</a>
+      : <p> </p>
       }
       <a href="#projects" className="navlink">PROJECTS</a>
       <a href="#contact" className="navlink">CONTACT</a>
