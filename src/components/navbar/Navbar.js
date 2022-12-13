1d9c514a6c4a2fa949a5a7ef23586d16917fbf0e
YuvrajM108 <yuvrajmanoo1@gmail.com>
2022-12-13 19:11:24 -0400

    Add condition to change navbar color when scroll down

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,28 +1,42 @@
 import React, { useState } from 'react';
 
 const Navbar = () => {
-  const [about, setAbout] = useState(false);
+  const [about, setAbout] = useState(0);
 
   const showAbout = () => {
-    if (window.scrollY >= 500) {
-      setAbout(true);
+    if (window.scrollY >= 50) {
+      setAbout(1);
+    }
+    else if (window.scrollY >= 500) {
+      setAbout(2);
     } else {
-      setAbout(false);
+      setAbout(0);
     }
   };
 
   window.addEventListener('scroll', showAbout);
-
-  return (
-    <nav className="navbar">
-      {
-      about ? <a href="#bio" className="navlink">ABOUT ME</a>
-        : <p className="hiddenlink">ABOUT ME</p>
-      }
-      <a href="#projects" className="navlink">PROJECTS</a>
-      <a href="#contact" className="navlink">CONTACT</a>
-    </nav>
-  );
+  
+  if (about === 0) {
+    return (
+      <nav className="navbar-grey">
+        <p className="hiddenlink">ABOUT ME</p>
+        <a href="#projects" className="navlink">PROJECTS</a>
+        <a href="#contact" className="navlink">CONTACT</a>
+      </nav>
+    );
+  }
+  else {
+    return (
+      <nav className="navbar">
+        {
+        about === 2 ? <a href="#bio" className="navlink">ABOUT ME</a>
+          : <p className="hiddenlink">ABOUT ME</p>
+        }
+        <a href="#projects" className="navlink">PROJECTS</a>
+        <a href="#contact" className="navlink">CONTACT</a>
+      </nav>
+    );
+  }
 };
 
 export default Navbar;
