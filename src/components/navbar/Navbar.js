159484a7bef5d2c6cd613cdeaad592a0e4709b83
YuvrajM108 <yuvrajmanoo1@gmail.com>
2022-12-13 19:33:35 -0400

    Attempt to fix remaining linter errors

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -17,21 +17,23 @@ const Navbar = () => {
   window.addEventListener('scroll', showAbout);
 
   return (
-    about === 0 ? (<nav className="navbar-grey">
-      <p className="hiddenlink">ABOUT ME</p>
-      <a href="#projects" className="navlink">PROJECTS</a>
-      <a href="#contact" className="navlink">CONTACT</a>
-    </nav>
-    ) :
-    (<nav className="navbar">
-      {
-      about === 2 ? <a href="#bio" className="navlink">ABOUT ME</a>
-        : <p className="hiddenlink">ABOUT ME</p>
-      }
+    about === 0 ? (
+      <nav className="navbar-grey">
+        <p className="hiddenlink">ABOUT ME</p>
         <a href="#projects" className="navlink">PROJECTS</a>
         <a href="#contact" className="navlink">CONTACT</a>
       </nav>
-    )
+      ) :
+      (
+      <nav className="navbar">
+        {
+          about === 2 ? <a href="#bio" className="navlink">ABOUT ME</a>
+          : <p className="hiddenlink">ABOUT ME</p>
+        }
+          <a href="#projects" className="navlink">PROJECTS</a>
+          <a href="#contact" className="navlink">CONTACT</a>
+      </nav>
+      )
   );
 };
 
